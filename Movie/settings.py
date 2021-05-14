b4b014e1c963e813ebd9006f71f73f6dcfb3a4e8
äniketsonu <56171163+aniketsonu@users.noreply.github.com>
2021-05-14 13:30:59 +0530

    Initial commit

--- Movie/settings.py
+++ Movie/settings.py
@@ -15,6 +15,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Quick-start development settings - unsuitable for production
