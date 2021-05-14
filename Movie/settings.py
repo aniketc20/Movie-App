1e1819084b250034c4bc6a1ed99ab791fb48d945
äniketsonu <56171163+aniketsonu@users.noreply.github.com>
2021-05-14 13:33:26 +0530

    Initial commit

--- Movie/settings.py
+++ Movie/settings.py
@@ -129,7 +129,7 @@ STATICFILES_DIRS = [
 ]
 STATIC_URL = '/static/'
 #  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 import dj_database_url 
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
